97aab44ab176fff13e0f5caf92a3bfa1823fcb7f
CJH-James <qaz75203code.gmail.com>
2025-09-18 00:35:04 +0800

    move file and rename

--- epub_convert/t2s.py
+++ epub_convert/t2s.py
@@ -1,7 +1,7 @@
 import zipfile
 import opencc
 from pathlib import Path
-from .commonFunc import findTargetFileList, mkdir, chkdir
+from ._libs._commonFunc import findTargetFileList, mkdir, chkdir
 
 # only initailize OpenCC once, or it would be very slow
 converter = opencc.OpenCC(config="t2s")
